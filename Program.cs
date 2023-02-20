a8be99c3cc51bbf37053f64b0c57b7899cc136e0
Emre <Emre@DESKTOP-H3LPS41>
2023-02-20 11:50:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1= new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir Karpuzu";
+
+
+            Urun[] urunler = new Urun[] { urun1, urun2, };
+
+
+            foreach (Urun urun in urunler)
+
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati); 
+                Console.WriteLine(urun.Aciklama);    
+
+            }
+
+
+            Console.WriteLine("################");
+
+            SepetManager sepetManager= new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yesil Armut", 12,5);
+            sepetManager.Ekle2("Armut", "Yesil Armut", 12, 5);
+            sepetManager.Ekle2("Armut", "Yesil Armut", 12, 5);
+
+
+
+
+
+
+
+
+        }
+    }
+}
